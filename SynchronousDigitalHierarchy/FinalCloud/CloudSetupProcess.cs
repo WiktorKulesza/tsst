206675d7526d3f845b868c10ef9caad08d945365
Michał Przybylski <michalprzybylski92@gmail.com>
2015-05-01 18:38:26 +0200

    Chmura kablowa
    Chmura korzysta z asynchronicznych Socketów i przesyła dane z portu
    źródłowego oznaczonego w pliku konfiguracyjnym jako src na port docelowy
    ozanczony jako src

---
+++ SynchronousDigitalHierarchy/FinalCloud/CloudSetupProcess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TsstSdh
+{
+    class CloudSetupProcess
+    {
+        private String configurationFilePath;
+
+        public CloudSetupProcess(string configurationFileName)
+        {
+            string defaultDirectoryPath = Directory.GetCurrentDirectory();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(defaultDirectoryPath);
+            sb.Append("\\");
+            sb.Append(configurationFileName);
+
+            this.configurationFilePath = sb.ToString();
+        } 
+
+        public void  startCloudProcess () {
+            if (!File.Exists(configurationFilePath))
+            {
+                Console.WriteLine("Check your configuration file ");
+                Console.WriteLine("your default config file path is {0} ", configurationFilePath);
+                return;
+            }
+            Manager configManager = new Manager(configurationFilePath);
+            List<Link> links = configManager.createLinks();
+            List<Thread> threadList = wrapInThreads(links);
+            foreach (Thread thread in threadList)
+            {
+                thread.Start();
+            }
+        }
+        private List<Thread> wrapInThreads(List<Link> links)
+        {
+            List<Thread> threadList = new List<Thread>();
+            foreach (Link link in links)
+            {
+                Thread linkThread = new Thread(new ThreadStart(link.startListening));
+                threadList.Add(linkThread);
+            }
+            return threadList;
+        }
+     
+    }
+}
---
+++ SynchronousDigitalHierarchy/FinalCloud/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TsstSdh.SocketUtils;
+
+namespace TsstSdh
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            CloudSetupProcess setupProcess = new CloudSetupProcess("linkConfig.xml");
+            setupProcess.startCloudProcess();
+            Console.ReadLine();
+        }
+    }
+}
