fcd351c62547d1b013025b0c93892f2094f8e1b1
WiktorKulesza <alchemik93@gmail.com>
2015-05-11 00:15:27 +0200

    Transport-Module-wstepna-budowa
    jak narazie rozbiłem transport module na kolejne fragmenty stringa.
    musze jeszcze porobic zabezpieczenia i dokladnie przemyslec funkcje
    jakie bedziemy wykonywac na tych wiadomosciach, wtedy bedziecie mogli
    sobie znnich korzystac w obu wezlach

---
+++ SynchronousDigitalHierarchy/TransportModole/AU4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportModole
+{
+    class AU4
+    {
+        string aU4;
+
+        private void createAU4(VC4 vc4)
+        {
+            aU4 = Protect.protectAU4 + vc4.GetMessageVC4();
+        }
+    }
+}
---
+++ SynchronousDigitalHierarchy/TransportModole/Protect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportModole
+{
+    class Protect
+    {
+        static public string protectAU4 = "p1"; 
+    }
+}
